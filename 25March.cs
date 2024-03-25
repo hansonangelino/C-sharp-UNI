96f500df7733746c6c8466d9b3eab7fb0fe7d50e
Hanson Angelino <30595122+hansonangelino@users.noreply.github.com>
2024-03-25 11:05:42 +0800

    Update 25March.cs

--- 25March.cs
+++ 25March.cs
@@ -1,4 +1,3 @@
-
 namespace _25March
 {
     internal class Program
@@ -7,20 +6,36 @@ namespace _25March
         {
             
             Console.WriteLine("Class score");
-            int[] grades = {92, 88, 90, 93, 96 };
+            //Variable for fixed grades
+            //int[] grades = {92, 88, 90, 93, 96 };
+
+            //Variable for manual grade input
+            int grades = 0;
             string[] course = { "Chinese", "English", "Religion", "Art", "History" };
 
             double sum = 0;
             double average = 0;
 
+            /*
             int a, b, c, d, e;
             a = grades[0];
             b = grades[1];
             c = grades[2];
             d = grades[3];
             e = grades[4];
-            
-            //Output using For
+            */
+            //Input for manual score
+            string[] newGrades = Console.ReadLine().Split(' ');
+            //Output using For for manual grade input
+            for (int i = 0; i < newGrades.Length; i++)
+            {
+                grades = int.Parse(newGrades[i]);
+                Console.WriteLine($"{course[i]}: {grades}");
+            }
+
+
+            //Output using For variant 1
+            /*
             Console.WriteLine("The For loop: ");
             for (int i = 0; i < grades.Length; i++)
             {
@@ -32,10 +47,14 @@ namespace _25March
                 if (i < grades.Length)
                 {
                     Console.WriteLine(" ");
+                    //Manual Input Output
+                    Console.WriteLine(scrNew[0]);
                 }
             }
             Console.WriteLine($"Average score is: {average:F2}");
 
+            Console.WriteLine(scrNew[0]);
+            */
             //Output using foreach
             /*
             Console.WriteLine("The Foreach loop: ");
